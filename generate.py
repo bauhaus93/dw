2a0c0a83e98a8cb849453b570bfd2ba99dbbd2b8
Jakob <jakobfischer93@gmail.com>
2018-10-24 00:37:04 +0200

    Changed logger formating to match style used in main program.

--- generate.py
+++ generate.py
@@ -4,7 +4,7 @@ import sys
 import logging
 
 def setup_logger():
-    FORMAT = r"[%(asctime)-15s - %(levelname)s] %(message)s"
+    FORMAT = r"[%(asctime)-15s] %(levelname)s - %(message)s"
     DATE_FORMAT = r"%Y-%m-%d %H:%M:%S"
     logging.basicConfig(level = logging.INFO, format=FORMAT, datefmt = DATE_FORMAT)
 
