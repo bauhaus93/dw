7f6d4da1bf8be246c3edb07aa1106d7c6f8038cb
Jakob Fischer <JakobFischer93@gmail.com>
2018-11-13 00:14:27 +0100

    Some lib find changes.

--- cmake/FindPugiXML.cmake
+++ cmake/FindPugiXML.cmake
@@ -2,13 +2,13 @@
 
 find_library(PUGIXML_LIBRARIES
              NAMES pugixml
-             DOC "The PugiXML library")
+             PATHS /usr/lib /usr/local/lib D:/Libraries/pugixml/lib
+             PATH_SUFFIXES x64 x86)
 
-find_path(PUGIXML_INCLUDE_DIR 
+find_path(PUGIXML_INCLUDE_DIRS
           NAMES pugixml.hpp
-          HINTS /usr/include /usr/local/include
-          DOC "The PugiXML include directory")
+          PATHS /usr/include /usr/local/include D:/Libraries/pugixml/include)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(pugixml DEFAULT_MSG PUGIXML_LIBRARIES PUGIXML_INCLUDE_DIR)
-mark_as_advanced(PUGIXML_INCLUDE_DIR PUGIXML_LIBRARIES)
+find_package_handle_standard_args(pugixml DEFAULT_MSG PUGIXML_LIBRARIES PUGIXML_INCLUDE_DIRS)
+mark_as_advanced(PUGIXML_INCLUDE_DIRS PUGIXML_LIBRARIES)
