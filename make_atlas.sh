bc40e5b23d32e9aa06acd10510d8eb97c910d6c0
Jakob Fischer <JakobFischer93@gmail.com>
2018-11-15 12:48:11 +0100

    Added further chain replacement

--- make_atlas.sh
+++ make_atlas.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source $1/bin/activate
-python3 src/resource_generation/generate.py --xml-file $2 --result-dir $3 --render-script $4
+python3 $2 --xml-file $3 --result-dir $4 --render-script $5
